c984b9fa1348d4e9131129dd40c47579ce9050bf
vitoxint@gmail.com <vitoxint@gmail.com>
2021-12-04 18:29:27 -0300

    2nd commit

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,33 +1,25 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-import "./menu-tem.styles.scss";
+import "./menu-item.styles.scss";
 
-
-const MenuItem = ({ title , imageUrl , size , linkUrl , history , match }) => (
-
-    <div 
-        className={`${size} menu-item`}
-        onClick={ () => history.push(`${match.url}${linkUrl}`) }
-    >
-
-        <div  
-            className="background-image" 
-            style={{
-                backgroundImage : `url(${imageUrl})`
-            }} 
-        />
-
-        <div className="title">
-            <h1>{title.toUpperCase()} </h1>
-            <span className="subitle" >SHOP NOW</span>
-
-        </div>
-
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-
+  </div>
 );
 
-
-// design pattern : HOC ( High Order Component )
+// design pattern: HOC (HIGH ORDER COMPONENT)
 export default withRouter(MenuItem);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import FormImput from "../form-input/form-input.component";
+import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import { signInWithGoogle } from "../../firebase/firebase.utils";
@@ -8,69 +8,63 @@ import { signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: "",
-            password : ""
-
-        };
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-
-        this.setState({ email : "" , password: ""});
-        
-
-
+    this.state = {
+      email: "",
+      password: "",
     };
+  }
 
-    handleChange = event => {
-        const { value , name  } = event.target; // name , value
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        this.setState({ [name] : value });
+    this.setState({ email: "", password: "" });
+  };
 
-    };
+  handleChange = (event) => {
+    const { value, name } = event.target; // name, value
 
-    // design pattern: controlled input
-    render(){
-        return (
-            <div className="sign-in">
+    this.setState({ [name]: value });
 
-                <h2>I already have account</h2>
-                <span>Sign In with your email and password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormImput
-                        name="email"
-                        type="email"
-                        handleChange={this.handleChange}
-                        value={this.state.email}
-                        label="email"
-                        required
-                    />
-                    <FormImput
-                        name="password"
-                        type="password"
-                        handleChange={this.handleChange}
-                        value={this.state.password}
-                        label="password"
-                        required
-                    />
+    console.log(this.state);
+  };
 
-                    <div className="buttons">
-                        <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign In With Google</CustomButton>
-                    </div>
+  // design pattern: controlled input
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-
-
-                </form>
-            </div>
-
-        );
-    }
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            name="password" props
+            type="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
